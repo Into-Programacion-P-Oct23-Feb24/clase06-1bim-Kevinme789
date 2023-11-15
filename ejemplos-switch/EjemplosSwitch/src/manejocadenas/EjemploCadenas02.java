f4cda7b03ecf57eb1b80f9123c67ffb88839d0fd
kevin <porkevin743@gmail.com>
2023-11-15 15:36:04 -0500

    Actividades

--- ejemplos-switch/EjemplosSwitch/src/manejocadenas/EjemploCadenas02.java
+++ ejemplos-switch/EjemplosSwitch/src/manejocadenas/EjemploCadenas02.java
@@ -15,6 +15,7 @@ import java.util.Scanner;
 public class EjemploCadenas02 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        // Escanner entrada nos permite el ingreso de datos
         entrada.useLocale(Locale.US);
         String cadenaFinal = "Datos Ingresados\n";
         
