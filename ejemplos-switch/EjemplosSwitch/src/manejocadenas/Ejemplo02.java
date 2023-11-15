7bac9bf3832af38b856133013849279606ac10ca
kevin <porkevin743@gmail.com>
2023-11-15 15:19:52 -0500

    actividad

--- ejemplos-switch/EjemplosSwitch/src/manejocadenas/Ejemplo02.java
+++ ejemplos-switch/EjemplosSwitch/src/manejocadenas/Ejemplo02.java
@@ -13,8 +13,8 @@ public class Ejemplo02 {
     
     public static void main(String[] args) {
         String pais;
-        
         pais = "Ecuador";
+        // para mostrar en pantalla el primer caracter
         char letra = pais.charAt(0);
         System.out.printf("%s\n", letra);
         
